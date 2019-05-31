a627496aecfef19611ac60d4596ada69294f3556
René Vögeli <rvoegeli@rangee.com>
2019-05-31 10:59:16 +0200

    Fix function call

--- start.php
+++ start.php
@@ -10,7 +10,8 @@
  */
 elgg_register_event_handler('init', 'system', 'forum_init');
 
-function forum_init() {
+function forum_init()
+{
     // Create menu item
     $item = new ElggMenuItem("forum", elgg_echo("forum:forums"), "forumcategory/all");
     elgg_register_menu_item("site", $item);
@@ -38,7 +39,8 @@ function forum_init() {
     elgg_register_plugin_hook_handler('entity:icon:url', 'object', 'forum_set_icon_url');
 
     // Register widget types
-    elgg_register_widget_type('forumtopic', elgg_echo('forumtopic:widget:title'), elgg_echo('forumtopic:widget:description'));
+    elgg_register_widget_type('forumtopic', elgg_echo('forumtopic:widget:title'),
+        elgg_echo('forumtopic:widget:description'));
 
     // Extend css view
     elgg_extend_view('elgg.css', 'css/forum');
@@ -53,27 +55,33 @@ function forum_init() {
     elgg_register_entity_type('object', 'forumtopic');
 }
 
-function forum_set_url($hook, $type, $url, $params) {
+function forum_set_url($hook, $type, $url, $params)
+{
     $entity = elgg_extract("entity", $params);
     if (elgg_instanceof($entity, 'object', 'forumcategory')) {
         $friendly_title = elgg_get_friendly_title($entity->getDisplayName());
+
         return "forumcategory/view/{$entity->guid}/$friendly_title";
     }
     if (elgg_instanceof($entity, 'object', 'forum')) {
         $friendly_title = elgg_get_friendly_title($entity->getDisplayName());
+
         return "forum/view/{$entity->guid}/$friendly_title";
     }
     if (elgg_instanceof($entity, 'object', 'forumtopic')) {
         $friendly_title = elgg_get_friendly_title($entity->getDisplayName());
+
         return "forumtopic/view/{$entity->guid}/$friendly_title";
     }
     if (elgg_instanceof($entity, 'object', 'forumreply')) {
         $container = get_entity($entity->container_guid);
+
         return "forumtopic/view/" . $container->getGUID() . "/#forumreply_" . $entity->getGUID();
     }
 }
 
-function forum_entity_menu_setup($hook, $type, $return, $params) {
+function forum_entity_menu_setup($hook, $type, $return, $params)
+{
     if (elgg_in_context('widgets')) {
         return $return;
     }
@@ -91,25 +99,29 @@ function forum_entity_menu_setup($hook, $type, $return, $params) {
     }
     if (elgg_instanceof($entity, "object", "forumcategory")) {
         $options = array(
-            'name' => 'add_forum',
-            'text' => elgg_echo("forum:add"),
-            'href' => "forum/add/" . $entity->guid,
+            'name'     => 'add_forum',
+            'text'     => elgg_echo("forum:add"),
+            'href'     => "forum/add/" . $entity->guid,
             'priority' => 150,
         );
         $return[] = ElggMenuItem::factory($options);
     }
     if (elgg_instanceof($entity, "object", "forum")) {
         $url = $entity->getURL();
-        $item = new ElggMenuItem("topic_count", elgg_echo("forum:count:topics", array(forum_count_topics($entity))), $url);
+        $item = new ElggMenuItem("topic_count", elgg_echo("forum:count:topics", array(forum_count_topics($entity))),
+            $url);
         array_unshift($return, $item);
-        $item = new ElggMenuItem("reply_count", elgg_echo("forum:count:replies", array(forum_count_replies($entity))), $url);
+        $item = new ElggMenuItem("reply_count", elgg_echo("forum:count:replies", array(forum_count_replies($entity))),
+            $url);
         array_unshift($return, $item);
     }
     if (elgg_instanceof($entity, "object", "forumtopic")) {
         $url = $entity->getURL();
-        $item = new ElggMenuItem("reply_count", elgg_echo("forumtopic:count:replies", array(topic_count_replies($entity))), $url);
+        $item = new ElggMenuItem("reply_count",
+            elgg_echo("forumtopic:count:replies", array(topic_count_replies($entity))), $url);
         array_unshift($return, $item);
-        $item = new ElggMenuItem("topic_view", elgg_echo("forumtopic:count:views", array(topic_count_views($entity))), $url);
+        $item = new ElggMenuItem("topic_view", elgg_echo("forumtopic:count:views", array(topic_count_views($entity))),
+            $url);
         array_unshift($return, $item);
     }
     if (elgg_instanceof($entity, "object", "forumreply")) {
@@ -138,10 +150,12 @@ function forum_entity_menu_setup($hook, $type, $return, $params) {
             }
         }
     }
+
     return $return;
 }
 
-function forum_set_icon_url($hook, $type, $url, $params) {
+function forum_set_icon_url($hook, $type, $url, $params)
+{
     $entity = $params['entity'];
     $size = elgg_extract('size', $params, 'large');
     $subtype = $entity->getSubType();
@@ -152,52 +166,58 @@ function forum_set_icon_url($hook, $type, $url, $params) {
     if (in_array($subtype, $forum_subtypes)) {
         $url = elgg_get_site_url() . ("mod/forum/graphics/icons/{$size}.png");
         $url = elgg_trigger_plugin_hook('file:icon:url', 'override', $params, $url);
+
         return $url;
     }
 }
 
-function topic_count_replies($topic) {
+function topic_count_replies($topic)
+{
 
     return elgg_get_entities(array(
-        "type" => "object",
-        "subtype" => "forumreply",
+        "type"           => "object",
+        "subtype"        => "forumreply",
         "container_guid" => $topic->getGuid(),
-        "count" => true
+        "count"          => true
     ));
 }
 
-function topic_count_views($topic) {
-    return (int) $topic->views;
+function topic_count_views($topic)
+{
+    return (int)$topic->views;
 }
 
-function forum_count_topics($forum) {
+function forum_count_topics($forum)
+{
     return elgg_get_entities(array(
-        "type" => "object",
-        "subtype" => "forumtopic",
+        "type"           => "object",
+        "subtype"        => "forumtopic",
         "container_guid" => $forum->getGUID(),
-        "count" => true
+        "count"          => true
     ));
 }
 
-function forum_count_replies($forum) {
+function forum_count_replies($forum)
+{
     $forumtopics = elgg_get_entities(array(
-        "type" => "object",
-        "subtype" => "forumtopic",
+        "type"           => "object",
+        "subtype"        => "forumtopic",
         "container_guid" => $forum->getGUID(),
     ));
     foreach ($forumtopics as $topic) {
         $count += elgg_get_entities(array(
-            "type" => "object",
-            "subtype" => "forumreply",
+            "type"           => "object",
+            "subtype"        => "forumreply",
             "container_guid" => $topic->getGUID(),
-            "count" => true
+            "count"          => true
         ));
     }
 
     return $count ? $count : 0;
 }
 
-function forum_push_breadcrumbs($vars) {
+function forum_push_breadcrumbs($vars)
+{
     elgg_push_breadcrumb(elgg_echo("forum:forums"), "forumcategory/all");
     switch ($vars['subtype']) {
         case "forum":
@@ -244,12 +264,13 @@ function forum_push_breadcrumbs($vars) {
         case "forumcategory":
             switch ($vars[0]) {
                 case "add":
-                    elgg_push_breadcrumb(elgg_echo("forumcategory:add"), "forumcategory/add/", $vars[1]);
+                    elgg_push_breadcrumb(elgg_echo("forumcategory:add"), "forumcategory/add/" . $vars[1]);
                     break;
                 case "view":
                     $guid = $vars[1];
                     $category = get_entity($guid);
-                    elgg_push_breadcrumb(elgg_get_excerpt($category->getDisplayName()), "forumcategory/view/" . $vars[1]);
+                    elgg_push_breadcrumb(elgg_get_excerpt($category->getDisplayName()),
+                        "forumcategory/view/" . $vars[1]);
                     break;
                 case "edit":
                     $guid = $vars[1];
@@ -264,9 +285,10 @@ function forum_push_breadcrumbs($vars) {
     }
 }
 
-function forum_update_views($entity) {
-    $views = (int) $entity->views;
-    $views ++;
+function forum_update_views($entity)
+{
+    $views = (int)$entity->views;
+    $views++;
     $access = elgg_get_ignore_access();
     elgg_set_ignore_access();
     $entity->views = $views;
